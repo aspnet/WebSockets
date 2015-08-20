5d55533a1d80a5d6f49735ecf5b5abd1904ae703
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-08-20 13:16:20 -0700

    Tell users what to do with "AccessDenied" exceptions
    As per - http://stackoverflow.com/questions/1987749/why-do-i-get-accessdenied-when-trying-to-use-httplistener

--- test/TestServer/Program.cs
+++ test/TestServer/Program.cs
@@ -18,9 +18,29 @@ namespace TestServer
 
         private static async Task RunEchoServer()
         {
+            var username = Environment.GetEnvironmentVariable("USERNAME");
+            var userdomain = Environment.GetEnvironmentVariable("USERDOMAIN");
+            var prefix = "http://*:12345/";
+
             HttpListener listener = new HttpListener();
-            listener.Prefixes.Add("http://*:12345/");
-            listener.Start();
+            listener.Prefixes.Add(prefix);
+            try
+            {
+                listener.Start();
+            }
+            catch (HttpListenerException ex)
+            {
+                if (ex.ErrorCode == 5)
+                {
+                    Console.WriteLine("You need to run the following command:");
+                    Console.WriteLine("  netsh http add urlacl url={0} user={1}\\{2} listen=yes", prefix, userdomain, username);
+
+                    Console.ReadLine();
+                    return;
+                }
+
+                throw; 
+            }
             Console.WriteLine("Started");
 
             while (true)
