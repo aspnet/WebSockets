a956498ee43d1dd463eacf91d8fa932831eee601
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-02-05 09:45:25 -0800

    install VCForPython27 in AppVeyor

--- build/setup-wstest.ps1
+++ build/setup-wstest.ps1
@@ -1,5 +1,33 @@
 function has($cmd) { !!(Get-Command $cmd -ErrorAction SilentlyContinue) }
 
+# Download VCForPython27 if necessary
+$VendorDir = Join-Path (Get-Location) "vendor"
+
+if(!(Test-Path $VendorDir)) {
+    mkdir $VendorDir
+}
+
+$VirtualEnvDir = Join-Path (Get-Location) "virtualenv";
+$ScriptsDir = Join-Path $VirtualEnvDir "Scripts"
+$WsTest = Join-Path $ScriptsDir "wstest.exe"
+
+$VCPythonMsi = Join-Path $VendorDir "VCForPython27.msi"
+if(!(Test-Path $VCPythonMsi)) {
+    Write-Host "Downloading VCForPython27.msi"
+    Invoke-WebRequest -Uri https://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi -OutFile "$VCPythonMsi"
+}
+else {
+    Write-Host "Using VCForPython27.msi from Cache"
+}
+
+# Install VCForPython27
+Write-Host "Installing VCForPython27"
+
+# Launch this way to ensure we wait for msiexec to complete. It's a Windows app so it won't block the console by default.
+Start-Process msiexec "/i","$VCPythonMsi","/qn","/quiet","/norestart" -Wait
+
+Write-Host "Installed VCForPython27"
+
 # Install Python
 if(!(has python)) {
     choco install python2
@@ -13,16 +41,13 @@ if(!(has python)) {
 pip install virtualenv
 
 # Make a virtualenv in .virtualenv
-$VirtualEnvDir = Join-Path (Get-Location) ".virtualenv";
-
 virtualenv $VirtualEnvDir
 
-& "$VirtualEnvDir\Scripts\python" --version
-& "$VirtualEnvDir\Scripts\pip" --version
+& "$ScriptsDir\python" --version
+& "$ScriptsDir\pip" --version
 
 # Install autobahn into the virtualenv
-& "$VirtualEnvDir\Scripts\pip" install autobahntestsuite
+& "$ScriptsDir\pip" install autobahntestsuite
 
-# We're done. The travis config has already established the path to WSTest should be within the virtualenv.
-Get-ChildItem .$VirtualEnvDir/bin
-& "$VirtualEnvDir\Scripts\wstest" --version
+$Ver = & "$WsTest" --version
+Write-Host "Using wstest $Ver"
