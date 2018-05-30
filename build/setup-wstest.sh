0840b9cb686ff35aca84037ac42aa42e98f4d96f
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2018-05-30 14:25:20 -0700

    herp derp -L

--- build/setup-wstest.sh
+++ build/setup-wstest.sh
@@ -14,7 +14,7 @@ python --version
 # Install local virtualenv
 mkdir .python
 cd .python
-curl -O https://pypi.python.org/packages/d4/0c/9840c08189e030873387a73b90ada981885010dd9aea134d6de30cd24cb8/virtualenv-15.1.0.tar.gz
+curl -OL https://pypi.python.org/packages/d4/0c/9840c08189e030873387a73b90ada981885010dd9aea134d6de30cd24cb8/virtualenv-15.1.0.tar.gz
 tar xf virtualenv-15.1.0.tar.gz
 cd ..
 
